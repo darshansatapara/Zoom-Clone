412fbcf7b92c8f47f052bf04cf5059ce0cfa15e6
darshansatapara <109288106+darshansatapara@users.noreply.github.com>
2023-07-20 00:41:12 +0530

    add  server file and packege 1:11

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
+const { Socket } = require("dgram");
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const io=require('socketio')(server);
+const { v4: uuidv4 } = require("uuid");
 
+app.set("view engine", "ejs");
 
-app.set('view engine', 'ejs')
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
- res.render('room')
+  res.redirect(`/${uuidv4()}`);
 });
 
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+io.on('connection',socket=>{
+    socket.on('join-room',()=>{
+        console.log("joined room")
+    })
+})
 
 
 server.listen(3030);
