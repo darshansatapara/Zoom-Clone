f8bb14e9495dcbcefb20f7bd44309595616243e9
darshansatapara <109288106+darshansatapara@users.noreply.github.com>
2023-07-29 01:30:22 +0530

    last commite

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
 })
-
 app.get('/:room', (req, res) => {
+
   res.render('room', { roomId: req.params.room })
 })
 
